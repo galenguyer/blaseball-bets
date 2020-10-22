4800fe81a80bc7460ce3120c7f16c940fe45b993
Galen Guyer <galen@galenguyer.com>
2020-10-21 20:02:20 -0400

    bring up to base spec

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:12.16.2 as build-deps
+WORKDIR /usr/src/app
+COPY package.json yarn.lock ./
+RUN yarn
+RUN yarn browserslist --update-db
+COPY . ./
+RUN yarn build
+
+FROM docker.galenguyer.com/nginx-auto/nginx-react
+COPY --from=build-deps /usr/src/app/build /var/www/html/bets
+COPY nginx.conf /etc/nginx/nginx.conf
+RUN echo 'ok' > /var/www/html/index.html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useSWR from "swr";
+import logo from "./logo.svg";
+import "./App.css";
+
+const url = "https://blase.nyaa.gay/api/v1/latest/streamData";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { data: result, error } = useSWR(url);
+
+    if (error)
+        return (
+            <div className="App">
+                <h1>BlaseBets</h1>
+                <h1>An error occurred</h1>
+            </div>
+        );
+    if (!result)
+        return (
+            <div className="App">
+                <h1>BlaseBets</h1>
+                <h1>Loading latest data...</h1>
+            </div>
+        );
+
+    const day = result.value.games.sim.day + 1;
+
+    return (
+        <div className="App">
+            <h1>BlaseBets (Day {day})</h1>
+
+            <div className="Comment" style={{ marginBottom: "1rem" }}>
+                Created by MasterChief_John-117#1911 &nbsp;|&nbsp; Source available on{" "}
+                <a href="https://github.com/galenguyer/blaseball-wins">github!</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
